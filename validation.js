3e519387b416d325eb5a585da028b209002804d2
Tsaryk Mykola <90026719+TsNikolay@users.noreply.github.com>
2023-05-16 13:44:38 +0200

    Added ability to validate nested objects

--- validation.js
+++ validation.js
@@ -2,9 +2,15 @@ function validate(data, schema) {
   const errors = [];
 
   function validateField(fieldName, fieldValue, fieldSchema) {
-    if (fieldSchema.required && (fieldValue === undefined || fieldValue === null)) {
-      errors.push(`${fieldName} is required.`);
-      return;
+
+    if(fieldValue === undefined || fieldValue === null){
+      if(fieldSchema.optional){
+        return
+      }
+      else{
+        errors.push(`${fieldName} is required.`);
+        return;
+      }
     }
 
     if (fieldSchema.type && typeof fieldValue !== fieldSchema.type) {
@@ -22,6 +28,14 @@ function validate(data, schema) {
       return;
     }
 
+    if (fieldSchema.schema && typeof fieldValue === 'object') {
+      const nestedErrors = validate(fieldValue, fieldSchema.schema);
+      if (nestedErrors.length > 0) {
+        errors.push(...nestedErrors.map(error => `${fieldName}.${error}`));
+      }
+
+    }
+
     // Дополнительные проверки, в зависимости от ваших требований
   }
 
@@ -35,102 +49,142 @@ function validate(data, schema) {
   return errors;
 }
 
-const GroupSchema = {
-  title: {
-    required: true,
-    type: "string",
-    minLength: 2,
+// Схема для валидации тестовых данных
+const schemaWithNestedObject = {
+  name: { type: 'string', minLength: 1 },
+  age: { type: 'number', min: 18 },
+  address: {
+    type: 'object',
+    schema: {
+      street: {
+        schema: {
+          house: { type: 'string', minLength: 1},
+          floor: { type: 'number', minLength: 1 },
+          apartment: {type: 'number', minLength: 1, optional: true}
+        },
+      },
+      city: { type: 'string', minLength: 1 },
+    },
   },
+};
+
+// Тестовые данные
+const dataWithNestedObject = {
+  name: 'John Doe',
+  age: 25,
+  address: {
+    street: {
+      house: "Test name",
+      floor: 12
+    },
+    city: 'New York',
+  },
+};
+
+// Вызов функции validate
+const errors = validate(dataWithNestedObject, schemaWithNestedObject);
+
+// Проверка результатов
+if (errors.length > 0) {
+  console.error('Validation errors:', errors);
+} else {
+  console.log('Data is valid.');
+}
+
+
+/* Дополнительные схемы для проверок
+const GroupSchema = {
+    title: {
+        required: true,
+        type: "string",
+        minLength: 2,
+    },
 };
 
 const CreateProductSchema = {
-  title: {
-    required: true,
-    type: "string",
-    minLength: 2,
-  },
-  amount: {
-    required: true,
-    type: "number",
-    min: 10,
-  },
-  categoryId: {
-    required: true,
-    type: "string",
-    minLength: 2,
-  },
+    title: {
+        required: true,
+        type: "string",
+        minLength: 2,
+    },
+    amount: {
+        required: true,
+        type: "number",
+        min: 10,
+    },
+    categoryId: {
+        required: true,
+        type: "string",
+        minLength: 2,
+    },
 };
 
 const EditProductSchema = {
-  id: {
-    required: true,
-    type: "string",
-  },
-  title: {
-    type: "string",
-    minLength: 2,
-  },
-  amount: {
-    type: "number",
-    min: 10,
-  },
-  categoryId: {
-    type: "string",
-    minLength: 2,
-  },
+    id: {
+        required: true,
+        type: "string",
+    },
+    title: {
+        type: "string",
+        minLength: 2,
+    },
+    amount: {
+        type: "number",
+        min: 10,
+    },
+    categoryId: {
+        type: "string",
+        minLength: 2,
+    },
 };
 
 const CreateRecipeSchema = {
-  title: {
-    required: true,
-    type: "string",
-  },
-  description: {
-    required: true,
-    type: "string",
-  },
-  video: {
-    required: true,
-    type: "string",
-  },
-  products: {
-    type: "object",
-  },
+    title: {
+        required: true,
+        type: "string",
+    },
+    description: {
+        required: true,
+        type: "string",
+    },
+    video: {
+        required: true,
+        type: "string",
+    },
+    products: {
+        type: "object",
+    },
 };
 
 const EditRecipeSchema = {
-  id: {
-    required: true,
-    type: "string",
-  },
-  title: {
-    required: true,
-    type: "string",
-  },
-  description: {
-    required: true,
-    type: "string",
-  },
-  video: {
-    required: true,
-    type: "string",
-  },
-  products: {
-    type: "object",
-  },
+    id: {
+        required: true,
+        type: "string",
+    },
+    title: {
+        required: true,
+        type: "string",
+    },
+    description: {
+        required: true,
+        type: "string",
+    },
+    video: {
+        required: true,
+        type: "string",
+    },
+    products: {
+        type: "object",
+    },
 };
 
-const editReceipt = {
-  id: "12",
-  title: "Apple pie",
-  description: "Its a good receipt",
-  video: "link",
-  products: ["apple", "pear"],
+const editReceiptData = {
+    id: "12",
+    title: "Apple pie",
+    description: "It's a good receipt",
+    video: "link",
+    products: ["apple", "pear"],
 };
+*/
 
-const errors = validate(editReceipt, EditRecipeSchema);
-if (errors.length === 0) {
-  console.log("Data is valid.");
-} else {
-  console.log("Validation errors:", errors);
-}
+
